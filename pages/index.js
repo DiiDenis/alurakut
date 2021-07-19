50611384292132c45a16c92b7c9f3c61c9f58aa1
Denis Izaias Pereira <diidenis@hotmail.com>
2021-07-19 17:51:14 -0300

    tela de login

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import nookies from 'nookies'
+import jwt from 'jsonwebtoken'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import {ProfileRelationsBoxWrapper} from '../src/components/ProfileRelations'
@@ -36,7 +38,7 @@ function ProfileRelationsBox(propriedades){
           </li>
           ) 
         })}
-      </ul> teste */}
+      </ul>*/}
     </ProfileRelationsBoxWrapper>
   )
 }
@@ -60,8 +62,8 @@ function ListaLateralDireita(propriedades){
   )
 }
 
-export default function Home() { 
-  const githubUser = 'DiiDenis';
+export default function Home(props) { 
+  const githubUser = props.githubUser;
   const [comunidades, setComunidades] =  React.useState([]) 
   const pessoasFavoritas = [
     'juunegreiros', 
@@ -201,4 +203,33 @@ export default function Home() {
     </>
   )
  
+}
+
+export async function getServerSideProps(context){
+  const cookies = nookies.get(context) 
+  const token = cookies.USER_TOKEN
+  
+
+  const {isAuthenticated} = await fetch('https://alurakut.vercel.app/api/auth',{
+    headers:{
+        Authorization: token
+    }    
+  })
+  .then(resposta => resposta.json())
+  
+  if(!isAuthenticated){
+    return{
+      redirect:{
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const {githubUser} = jwt.decode(token)
+  return {
+    props: {
+      githubUser
+    },
+  }
 }
